34334cf26925f009dd0fe2a849d53617e37e5850
Buzlu <buzlu2667@gmail.com>
2025-11-05 23:03:50 +0300

    quickview responsive fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,7 +50,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white">
       {/* ✅ HERO SLIDER */}
-      <section className="relative w-full overflow-hidden mb-8 mt-6 sm:mt-0">
+      <section className="relative w-full overflow-hidden mb-8 mt-10 sm:mt-0">
       <div className="w-full h-[100vw] sm:h-[70vh] md:h-[80vh] lg:h-[80vh] sm:aspect-auto">
   <Swiper
     modules={[Autoplay, Pagination, Navigation]}
