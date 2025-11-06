5114d8843773d56c05c5aa653967310e0841826c
Buzlu <buzlu2667@gmail.com>
2025-11-06 09:48:04 +0300

    quickview responsive fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -217,7 +217,7 @@ async function handleLogin(e) {
   // ✅ Drawer'ı kapat (toast görünür olsun)
   setTimeout(() => {
   setSignupOpen(false);
-}, 5000); // ✅ 1.5 sn sonra kapanır
+}, 4500); // ✅ 1.5 sn sonra kapanır
 
   // ✅ Toast bildirimi gönderelim
   window.dispatchEvent(
@@ -225,7 +225,7 @@ async function handleLogin(e) {
       detail: {
         type: "info",
         text: "📨💛💫 E-posta adresine doğrulama bağlantısı gönderildi! Lütfen mailini kontrol et.",
-         duration: 16000
+         duration: 20000
       },
     })
   );
